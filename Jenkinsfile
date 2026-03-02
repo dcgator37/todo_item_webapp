51c0697bfb3e003ab551d869a53dbba41c925d52
dcgator37 <dcgator8@hotmail.com>
2026-03-01 22:19:17 -0500

    adding Jenkinsfile

---
+++ Jenkinsfile
@@ -0,0 +1,42 @@
+pipeline {
+    agent any
+
+    environment {
+        IMAGE_NAME = "todo-app"
+    }
+
+    stages {
+        stage('Checkout') {
+            steps {
+                checkout scm
+            }
+        }
+
+        stage('Build') {
+            steps {
+                sh 'mvn clean package'
+            }
+        }
+
+        stage('Test') {
+            steps {
+                sh 'mvn test'
+            }
+        }
+
+        stage('Docker Build') {
+            steps {
+                sh 'docker build -t $IMAGE_NAME .'
+            }
+        }
+    }
+
+    post {
+        success {
+            echo 'Build, tests, and Docker image completed successfully.'
+        }
+        failure {
+            echo 'Build failed.'
+        }
+    }
+}
--- src/test/java/database/TodoDatabaseTest.java
+++ src/test/java/database/TodoDatabaseTest.java
@@ -51,52 +51,16 @@ class TodoDatabaseTest {
 
     @Test
     void testAddAndRetrieve() throws Exception {
-        /*try {
-            Connection conn = getConnection();
-            Statement stmt = conn.createStatement();
-            stmt.execute("""
-            CREATE TABLE  todo (
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                text VARCHAR(255) NOT NULL
-            )
-        """);
-            stmt.close();
-            conn.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        TodoDatabase dao = new TodoDatabase();
-        dao.add("Test Item");*/
-
         assertTrue(
                 dao.findAll()
                         .stream()
                         .anyMatch(i -> i.getText().equals("Test Item"))
         );
-
-        //dao.delete(1);
     }
 
     @Test
     void testAddDeleteItem() throws Exception {
-        /*try {
-            Connection conn = getConnection();
-            Statement stmt = conn.createStatement();
-            stmt.execute("""
-            CREATE TABLE  todo (
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                text VARCHAR(255) NOT NULL
-            )
-        """);
-            stmt.close();
-            conn.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
-        TodoDatabase dao = new TodoDatabase();
-        dao.add("2nd Test Item");*/
         dao.delete(1);
         assertFalse(
                 dao.findAll()
